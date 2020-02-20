a247a92f988cf93e46c65e27b8c8e29729c1c77f
y-aurora <42402727+y-aurora@users.noreply.github.com>
2020-02-21 00:05:20 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+From phusion/baseimage:0.11
+MAINTAINER y-aurora
+
+ENV HOME="/root" LC_ALL="C.UTF-8" LANG="en_US.UTF-8" LANGUAGE="en_US.UTF-8" DEBIAN_FRONTEND="noninteractive" TERM="xterm"
+
+# Use baseimage-docker's init system
+CMD ["/sbin/my_init"]
+
+RUN apt-get update -qy \
+&& apt-get upgrade -qy \
+&& apt-get install --no-install-recommends -qy \
+	avahi-daemon \
+	avahi-utils \
+	bzr \
+	cups \
+	cups-pdf \
+	cups-filters \
+	google-cloud-print-connector \
+	hplip \
+	inotify-tools \
+	libcups2 \
+	libavahi-client3 \
+    libsnmp30 \
+	printer-driver-all \
+	printer-driver-hpcups \
+	printer-driver-hpijs \
+	printer-driver-pxljr \
+	python-cups \
+	python \
+	whois \	
+&& apt-get -qq -y autoclean \
+&& apt-get -qq -y autoremove \
+&& apt-get -qq -y clean
+
+
+COPY init.sh airprint-generate.py /tmp/
+RUN rm -rf /etc/service/sshd /etc/service/cron /etc/service/syslog-ng /etc/my_init.d/00_regen_ssh_host_keys.sh /var/lib/apt/lists/* /var/tmp/* || true \
+&& mv -f /usr/lib/cups/backend/parallel /usr/lib/cups/backend-available/ || true \
+&& mv -f /usr/lib/cups/backend/serial /usr/lib/cups/backend-available/ || true \
+&& chmod +x /tmp/init.sh && chmod +x /tmp/airprint-generate.py && /tmp/init.sh
+
+# Export volumes
+VOLUME /config /etc/cups/ /var/log/cups /var/spool/cups /var/cache/cups
+EXPOSE 631
---
+++ init.sh
@@ -0,0 +1,86 @@
+mkdir -p /etc/my_init.d
+cp /tmp/init.sh /opt/
+cp /tmp/airprint-generate.py /opt/
+# include config files
+cat <<'EOT' >/etc/my_init.d/config.sh
+#!/bin/bash
+mkdir -p /config/cups /config/spool /config/logs /config/cache /config/cups/ssl /config/cups/ppd /config/cloudprint /config/avahi
+
+# Copy missing config files
+cd /etc/cups
+for f in *.conf ; do 
+  if [ ! -f "/config/cups/${f}" ]; then
+    cp ./${f} /config/cups/
+  fi
+done
+EOT
+chmod +x /etc/my_init.d/config.sh
+
+
+# Add cups to runit
+mkdir -p /etc/service/cups
+cat <<'EOT' >/etc/service/cups/run
+#!/bin/sh
+if [ -n "$CUPS_USER_ADMIN" ]; then
+  if [ $(grep -ci $CUPS_USER_ADMIN /etc/shadow) -eq 0 ]; then
+    useradd $CUPS_USER_ADMIN --system -G root,lpadmin --no-create-home --password $(mkpasswd $CUPS_USER_PASSWORD)
+  fi
+fi
+exec /usr/sbin/cupsd -f -c /config/cups/cupsd.conf
+cupsctl --remote-admin --remote-any --share-printers
+EOT
+chmod +x /etc/service/cups/run
+
+
+# Add AirPrint to runit
+mkdir -p /etc/service/airprint
+cat <<'EOT' > /etc/service/airprint/run
+#!/bin/bash
+
+while [[ $(curl -sk localhost:631 >/dev/null; echo $?) -ne 0 ]]; do
+  sleep 1
+done
+
+/opt/airprint-generate.py -d /config/avahi
+
+inotifywait -m /config/cups/ppd -e create -e moved_to -e close_write|
+    while read path action file; do
+        echo "Printer ${file} modified, reloading Avahi services."
+        /opt/airprint-generate.py -d /config/avahi
+    done
+EOT
+
+cat <<'EOT' > /etc/service/airprint/finish
+#!/bin/bash
+rm -rf /config/avahi/AirPrint*
+EOT
+chmod +x /etc/service/airprint/*
+
+
+# make avahi autostart
+cat <<'EOT' >/etc/my_init.d/avahi.sh
+#!/bin/bash
+mkdir -p /run/avahi-daemon
+chown -R avahi:avahi /run/avahi-daemon/
+service dbus restart
+rm -f /run/avahi-daemon/pid
+avahi-daemon -D --no-chroot
+EOT
+chmod +x /etc/my_init.d/avahi.sh
+
+# make gcp autostart
+mkdir -p /etc/service/gcp
+cat <<'EOT' >/etc/service/gcp/run
+#!/bin/sh
+rm -f /tmp/cloud-print-connector-monitor.sock
+if [ ! -f "/config/cloudprint/gcp-cups-connector.config.json" ]; then
+	cd /config/cloudprint && gcp-connector-util init --local-printing-enable --cloud-printing-enable=false
+fi
+if [ -r /config/cloudprint/gcp-cups-connector.config.json ]
+  then
+    exec gcp-cups-connector --config-filename /config/cloudprint/gcp-cups-connector.config.json
+  else
+    exec gcp-cups-connector
+fi
+EOT
+chmod +x /etc/service/gcp/run
