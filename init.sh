f187858c0a3e42c2f00bc766a6eef53bfb3ae602
yaurora <42402727+yaurora@users.noreply.github.com>
2020-11-13 21:42:30 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -22,8 +22,9 @@ cat <<'EOT' >/etc/service/cups/run
 #!/bin/sh
 if [ -n $CUPS_USER_ADMIN ]; then
   if [ $(grep -ci $CUPS_USER_ADMIN /etc/shadow) -eq 0 ]; then
-    #useradd $CUPS_USER_ADMIN --system -G root,lpadmin -M --password $(echo $CUPS_USER_PASSWORD |openssl passwd -1 -stdin)
-    useradd -M -G root,lpadmin -p $(perl -e 'print crypt($ARGV[0], "password")' $CUPS_USER_PASSWORD) $CUPS_USER_ADMIN
+    #useradd $CUPS_USER_ADMIN --system -G root,lpadmin -M --password $(openssl passwd -1 $CUPS_USER_PASSWORD)
+    useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' $CUPS_USER_PASSWORD) $CUPS_USER_ADMIN
+
   fi
 fi
 exec /usr/sbin/cupsd -f -c /config/cups/cupsd.conf -s /config/cups/cups-files.conf
